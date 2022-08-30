0c8545c685eb376f67d1a93a90c79b3034fc2b9c
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2022-08-31 00:37:09 +0430

    first

---
+++ _next/static/KsSmTq8PL8dDFj-sxOeGj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/811-f59c3d737dcb590c.js","static/chunks/pages/index-f30251369d17ca3c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
