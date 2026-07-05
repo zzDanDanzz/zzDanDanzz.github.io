5481a2550066921f7102d9620f4a594ecd4c9d4c
Danial Zahmatkesh <dansfancymaths@gmail.com>
2026-07-05 14:08:35 +0330

    Install @tanstack/react-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
